cb1508a83cfaa3eae9985d130386b3dc9f8af3de
Bayashat <baiashat2002@gmail.com>
2023-08-01 22:23:48 +0600

    Update DigitsSumCalculator.java

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,11 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+        String s = String.valueOf(number);
+        int result = 0;
+        for(int i = 0; i < s.length(); i++){
+            result += Character.getNumericValue(s.charAt(i));
+        }
+        System.out.println(result);
     }
-
 }
