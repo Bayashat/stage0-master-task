3d7d92b20ee12f824f5af59205e52623d93968e5
Bayashat <baiashat2002@gmail.com>
2023-08-01 22:37:53 +0600

    Update IntegerDivider.java

--- src/main/java/com/epam/conditions/IntegerDivider.java
+++ src/main/java/com/epam/conditions/IntegerDivider.java
@@ -3,6 +3,13 @@ package com.epam.conditions;
 public class IntegerDivider {
 
     public void printCompletelyDivided(int dividend, int divider) {
+        int quotient = dividend / divider;
+        int result = quotient * divider;
+        if(result == dividend){
+            System.out.println("can be divided completely");
+        }
+        else{
+            System.out.println("cannot be divided completely");
+        }
     }
-
 }
