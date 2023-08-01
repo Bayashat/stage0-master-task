298cde0e1f04b0f3704630b7b85d2da809556d46
Bayashat <baiashat2002@gmail.com>
2023-08-01 22:19:46 +0600

    Update NumberReverter.java

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,12 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        String s = String.valueOf(number);
+        String s2 = "";
+        for(int i = 0; i < s.length(); i++){
+            s2 = s.charAt(i) + s2;
+        }
+        int result = Integer.parseInt(s2);
+        System.out.println(result);
     }
-
 }
